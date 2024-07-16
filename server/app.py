dd060899e990367d918c97ada7861c781e5b98ab
Taylor Ng <taylormng95@gmail.com>
2024-07-16 19:54:11 -0400

    completes plant lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,29 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plants_dict = [plant.to_dict() for plant in plants]
+        return make_response(plants_dict, 200)
+    
+    def post(self):
+        data = request.get_json()
+        name = data.get('name')
+        image = data.get('image')
+        price = data.get('price')
+        new_plant = Plant(name=name, image=image, price=price)
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(new_plant.to_dict(), 201)
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant_dict = Plant.query.filter(Plant.id == id).first().to_dict()
+        return make_response(plant_dict, 200)
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
